d270e72d572b7d6ffc982736fee8ff8a296fd0de
LuckyHookin <luckyhookin@qq.com>
2021-02-05 21:38:52 +0800

    *

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify';
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const instance = axios.create({
+  baseURL: 'https://a2b4be01-6c86-4855-b330-a7a311dbf588.bspapp.com/http/api/',
+})
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, instance)
+
+
 new Vue({
+  router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    document.dispatchEvent(new Event('render-event'))
+  }
 }).$mount('#app')
